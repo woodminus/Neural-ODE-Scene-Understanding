3daf9cf3b62fee5afdcb83cd5cb482cfc0c819b9
woodminus <160528767+woodminus@users.noreply.github.com>
2023-10-12 13:38:54 +0000

    Fix background

--- lib/fpn/roi_align/modules/roi_align.py
+++ lib/fpn/roi_align/modules/roi_align.py
@@ -30,4 +30,13 @@ class RoIAlignAvg(Module):
 
 class RoIAlignMax(Module):
     def __init__(self, aligned_height, aligned_width, spatial_scale):
-        s
+        super(RoIAlignMax, self).__init__()
+
+        self.aligned_width = int(aligned_width)
+        self.aligned_height = int(aligned_height)
+        self.spatial_scale = float(spatial_scale)
+
+    def forward(self, features, rois):
+        x =  RoIAlignFunction(self.aligned_height+1, self.aligned_width+1,
+                                self.spatial_scale)(features, rois)
+        return max_pool2d(x, kernel_size=2, stride=1)
