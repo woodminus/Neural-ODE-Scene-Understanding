3ab1a633ea64f49e31c315b85aea468134e897d7
woodminus <160528767+woodminus@users.noreply.github.com>
2023-10-26 16:24:56 +0000

    Updating readme.

--- lib/fpn/roi_align/src/roi_align_cuda.c
+++ lib/fpn/roi_align/src/roi_align_cuda.c
@@ -48,4 +48,29 @@ int roi_align_backward_cuda(int crop_height, int crop_width, float spatial_scale
     float * grads_ptr = THCudaTensor_data(state, top_grad);
     float * boxes_ptr = THCudaTensor_data(state, rois);
 
-    float * grads_image_ptr = THCudaTensor
+    float * grads_image_ptr = THCudaTensor_data(state, bottom_grad);
+
+    // Number of ROIs
+    int num_boxes = THCudaTensor_size(state, rois, 0);
+    int size_rois = THCudaTensor_size(state, rois, 1);
+    if (size_rois != 5)
+    {
+        return 0;
+    }
+
+    // batch size
+    int batch = THCudaTensor_size(state, bottom_grad, 0);
+    // data height
+    int image_height = THCudaTensor_size(state, bottom_grad, 2);
+    // data width
+    int image_width = THCudaTensor_size(state, bottom_grad, 3);
+    // Number of channels
+    int depth = THCudaTensor_size(state, bottom_grad, 1);
+
+    cudaStream_t stream = THCState_getCurrentStream(state);
+
+    ROIAlignBackwardLaucher(
+        grads_ptr, boxes_ptr, num_boxes, batch, image_height, image_width,
+        crop_height, crop_width, depth, grads_image_ptr, stream);
+    return 1;
+}
